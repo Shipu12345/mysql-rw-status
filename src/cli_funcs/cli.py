fea40fd63e46e66877f3548721b750967d20013c
shipu <shipu@polygontech.xyz>
2022-09-12 11:54:21 +0600

    Initially finished: phase 1

--- src/cli_funcs/cli.py
+++ src/cli_funcs/cli.py
@@ -17,7 +17,10 @@ from src.connections import Connections
 from src.DAL.DBStatus import DBStatus
 import time
 import os
-import keyboard
+from pynput import keyboard
+from threading import Thread
+
+
 
 app = typer.Typer()
 
@@ -36,9 +39,7 @@ def fetch_pass(
 
 
 @app.command()
-def fetch_host(
-    host: str = typer.Option("127.0.0.1", "--host", "-h")
-) -> None:
+def fetch_host(host: str = typer.Option("127.0.0.1", "--host", "-h")) -> None:
     return host
 
 
@@ -72,87 +73,127 @@ def show_table(
     user: str = typer.Option("root", "--user", "-u"), passw=fetch_pass(), host=fetch_host(), port=fetch_port(), database=fetch_database()
 ) -> None:
 
-    while True:
-        if keyboard.is_pressed("q"):
-            print("q pressed, ending loop")
-            break
-    
+    typer.secho(f"-----Press 'Esc' to stop the loop...-----", fg=typer.colors.GREEN, bold=True)
+
+    def on_press(key, abortKey='esc'):    
         try:
-            mysql_ob = MysqlConfig(user=user, passwd=passw, host=host, port=port, database=database)
-            mysql_conn = Connections().get_mysql_conn(mysql_ob)
-            db_status = DBStatus(mysql_conn)
-            start_time = time.time()
-            data_dict = db_status.check_mysql_status()
-            end_time = time.time()
-            elasped_time = end_time - start_time
+            k = key.char  # single-char keys
         except:
-            typer.secho(
-                f'Error: "{ERRORS[1]}"',
-                fg=typer.colors.RED,
-            )
-            raise typer.Exit(1)
+            k = key.name  # other keys    
+
         
+        typer.secho(f"----- pressed {k}! -----", fg=typer.colors.MAGENTA, bold=True)
+        if k == abortKey:
+            typer.secho(f"----- Aborted! -----", fg=typer.colors.RED, bold=True)
+            return False  # stop listener
 
-        start_time = datetime.datetime.fromtimestamp(start_time)
-        start_time_str = start_time.strftime("%d-%m-%y %H:%M:%S")
-        terminal_width, _ = os.get_terminal_size(0)
+    def db_status_loop(user, passw, host, port, database):
+        x=-1
+        bottom_line =False
+        while True:
+            x+=1
+            try:
+                mysql_ob = MysqlConfig(user=user, passwd=passw, host=host, port=port, database=database)
+                mysql_conn = Connections().get_mysql_conn(mysql_ob)
+                db_status = DBStatus(mysql_conn)
+                start_time = time.time()
+                data_dict = db_status.check_mysql_status()
+                end_time = time.time()
+                elasped_time = end_time - start_time
+            except:
+                typer.secho(
+                    f'Error: "{ERRORS[1]}"',
+                    fg=typer.colors.RED,
+                )
+                raise typer.Exit(1)
 
-        if mysql_conn.is_connected():
-            status = "Okay"
-        else:
-            status = "Down"
+            start_time = datetime.datetime.fromtimestamp(start_time)
+            start_time_str = start_time.strftime("%d-%m-%y %H:%M:%S")
+            terminal_width, _ = os.get_terminal_size(0)
+            terminal_width -=10
 
-        elasped_time_str = f"{float(elasped_time*1000) :2f}"
+            if mysql_conn.is_connected():
+                status = "Okay"
+            else:
+                status = "Down"
 
-        start_time_str_space, start_time_str_space_val = space_lenth("Start Time", start_time_str)
-        status_space, status_space_val = space_lenth("Status", status)
-        elasped_time_str_space, elasped_time_str_space_val = space_lenth("Response Time", elasped_time_str)
+            elasped_time_str = f"{float(elasped_time*1000) :2f}"
 
-        user_str_space, user_str_space_val = space_lenth("User", data_dict["user"])
-        host_str_space, host_str_space_val = space_lenth("Host", data_dict["host"])
-        port_str_space, port_str_space_val = space_lenth("Port", port)
-        read_only_str_space, read_only_str_space_val = space_lenth("Read Only", str(data_dict["read_only"]))
-        super_read_only_str_space, super_read_only_str_space_val = space_lenth("Super Read Only", str(data_dict["super_read_only"]))
+            start_time_str_space, start_time_str_space_val = space_lenth("Start Time", start_time_str)
+            status_space, status_space_val = space_lenth("Status", status)
+            elasped_time_str_space, elasped_time_str_space_val = space_lenth("Response Time", elasped_time_str)
 
-        columns = (
-            f"{' '*start_time_str_space}Start Time{' '*start_time_str_space}",
-            f"|{' '*status_space}Status{' '*status_space}",
-            f"|{' '*elasped_time_str_space}Response Time{' '*elasped_time_str_space} ",
-            f"|{' '*user_str_space}User{' '*user_str_space}",
-            f"|{' '*host_str_space}Host{' '*host_str_space}",
-            f"|{' '*port_str_space}Port{' '*port_str_space}",
-            f"|{' '*read_only_str_space}read_only{' '*read_only_str_space}",
-            f"|{' '*super_read_only_str_space}Super Read Only{' '*super_read_only_str_space}",
-        )
+            user_str_space, user_str_space_val = space_lenth("User", data_dict["user"])
+            host_str_space, host_str_space_val = space_lenth("Host", data_dict["host"])
+            port_str_space, port_str_space_val = space_lenth("Port", port)
+            read_only_str_space, read_only_str_space_val = space_lenth("Read Only", str(data_dict["read_only"]))
+            super_read_only_str_space, super_read_only_str_space_val = space_lenth("Super Read Only", str(data_dict["super_read_only"]))
 
-        headers = "".join(columns)
-        typer.secho(headers, fg=typer.colors.BLUE, bold=True)
-        typer.secho("-" * terminal_width, fg=typer.colors.BLUE)
+            columns = (
+                f"{' '*start_time_str_space}Start Time{' '*start_time_str_space}",
+                f"|{' '*status_space}Status{' '*status_space}",
+                f"|{' '*elasped_time_str_space}Response Time{' '*elasped_time_str_space} ",
+                f"|{' '*user_str_space}User{' '*user_str_space}",
+                f"|{' '*host_str_space}Host{' '*host_str_space}",
+                f"|{' '*port_str_space}Port{' '*port_str_space}",
+                f"|{' '*read_only_str_space}read_only{' '*read_only_str_space}",
+                f"|{' '*super_read_only_str_space}Super Read Only{' '*super_read_only_str_space}",
+            )
 
-        typer.secho(f"{' '*start_time_str_space_val}{start_time_str}{' '*start_time_str_space_val} |", fg=typer.colors.BLUE, bold=True, nl=False)
-        if status == "Okay":
-            typer.secho(f"{' '*status_space_val}{status}{' '*status_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False)
-        else:
-            typer.secho(f"{' '*status_space_val}{status}{' '*status_space_val}|", fg=typer.colors.RED, bold=True, nl=False)
+            headers = "".join(columns)
 
-        typer.secho(
-            f"{' '*elasped_time_str_space_val}{float(elasped_time*1000) :2f}{' '*elasped_time_str_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False
-        )
-        typer.secho(f"{' '*user_str_space_val}{data_dict['user']}{' '*user_str_space_val} |", fg=typer.colors.BLUE, bold=True, nl=False)
-        typer.secho(f"{' '*host_str_space_val}{data_dict['host']}{' '*host_str_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False)
-        typer.secho(f"{' '*port_str_space_val}{port}{' '*port_str_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False)
-        typer.secho(
-            f"{' '*read_only_str_space_val}{str(data_dict['read_only'])}{' '*read_only_str_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False
-        )
-        typer.secho(
-            f"{' '*super_read_only_str_space_val}{str(data_dict['super_read_only'])}{' '*super_read_only_str_space_val}",
-            fg=typer.colors.BLUE,
-            bold=True,
-            nl=False,
-        )
+            if x%10==0:
+                print('\n')
+                typer.secho(headers, fg=typer.colors.BRIGHT_CYAN, bold=True)
+                typer.secho("-" * terminal_width, fg=typer.colors.BLUE)
+                x=1
+                bottom_line = True
 
-        print()
-        typer.secho("-" * terminal_width + "\n", fg=typer.colors.BRIGHT_BLUE)
+            typer.secho(f"{' '*start_time_str_space_val}{start_time_str}{' '*start_time_str_space_val} |", fg=typer.colors.BLUE, bold=True, nl=False)
+            if status == "Okay":
+                typer.secho(f"{' '*status_space_val}{status}{' '*status_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False)
+            else:
+                typer.secho(f"{' '*status_space_val}{status}{' '*status_space_val}|", fg=typer.colors.RED, bold=True, nl=False)
+
+            typer.secho(
+                f"{' '*elasped_time_str_space_val}{float(elasped_time*1000) :2f}{' '*elasped_time_str_space_val}|",
+                fg=typer.colors.BLUE,
+                bold=True,
+                nl=False,
+            )
+            typer.secho(f"{' '*user_str_space_val}{data_dict['user']}{' '*user_str_space_val} |", fg=typer.colors.BLUE, bold=True, nl=False)
+            typer.secho(f"{' '*host_str_space_val}{data_dict['host']}{' '*host_str_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False)
+            typer.secho(f"{' '*port_str_space_val}{port}{' '*port_str_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False)
+            typer.secho(
+                f"{' '*read_only_str_space_val}{str(data_dict['read_only'])}{' '*read_only_str_space_val}|", fg=typer.colors.BLUE, bold=True, nl=False
+            )
+            typer.secho(
+                f"{' '*super_read_only_str_space_val}{str(data_dict['super_read_only'])}{' '*super_read_only_str_space_val}",
+                fg=typer.colors.BLUE,
+                bold=True,
+                nl=False,
+            )
+
+            print()
+            if x != 1 and bottom_line:
+                typer.secho("-" * terminal_width + "\n", fg=typer.colors.BRIGHT_BLUE)
+    
+            bottom_line = False
+            time.sleep(1)
+
+
+    while True:
+        listener = keyboard.Listener(on_press=on_press)
+        listener.start()  # start to listen on a separate thread
+
+        # start thread with loop
+        Thread(target=db_status_loop, args=(user, passw, host, port, database), name='db_status_loop', daemon=True).start()
+
+        listener.join() # wait for abortKey
+        break;
+
+ 
+        
 
 
 @app.callback()
--- src/connections.py
+++ src/connections.py
@@ -3,7 +3,7 @@ from mysql import connector
 
 
 class Connections:
-    def get_mysql_cursor(self, config: MysqlConfig):
+    def get_mysql_conn(self, config: MysqlConfig):
 
         self.conn = connector.connect(host=config.host, user=config.user, passwd=config.passwd, database=config.database, ssl_disabled=True)
         return self.conn
